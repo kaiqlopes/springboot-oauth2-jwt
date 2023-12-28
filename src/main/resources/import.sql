ca1ce7e9266cc87ff044fac1b6a3f9a7d762157b
Kaique Lopes <kai-quelopes@hotmail.com>
2023-12-28 14:38:48 -0300

    PasswordEncoder implementation

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$BpgT2UZ87QGCTTzoTk5uU.QCPG45e1XDtRfKg/uHWVD0QaAiUuJ7K');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$BpgT2UZ87QGCTTzoTk5uU.QCPG45e1XDtRfKg/uHWVD0QaAiUuJ7K');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
